0176ea6d918059429c88fe6e01784292cd943a13
SUCCESS <khalid.saifullah0012@gmail.com>
2024-10-12 20:01:35 +0600

    Added all projects file

---
+++ src/main/java/com/microtech/UserDataValidationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.microtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataValidationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDataValidationSystemApplication.class, args);
+	}
+
+}
