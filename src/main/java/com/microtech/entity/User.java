617175bad7c0553c6a74828757d340dd7ff7560f
SUCCESS <khalid.saifullah0012@gmail.com>
2024-10-12 22:22:50 +0600

    Added updated code

---
+++ src/main/java/com/microtech/entity/User.java
@@ -0,0 +1,28 @@
+package com.microtech.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+    private String streetAddress;
+    private Date dob;
+    private String gender;
+
+}
---
+++ src/main/java/com/microtech/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.microtech.repository;
+
+import com.microtech.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
